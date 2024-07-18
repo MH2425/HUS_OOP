50e2eb30484d5332e7e8058ff90650c22ac33b81
MH2425 <moongazing@gmail.com>
2024-07-18 10:47:28 +0700

    Completed an easy exercise

--- src/hus/oop/lab01/exercises_on_loop_and_decisions/ExtractDigits.java
+++ src/hus/oop/lab01/exercises_on_loop_and_decisions/ExtractDigits.java
@@ -1,10 +1,15 @@
-//package hus.oop.lab01.exercises_on_loop_and_decisions;
-//
-//import java.util.Scanner;
-//public class ExtractDigits {
-//    static Scanner scanner = new Scanner(System.in);
-//    public static void main(String[] args) {
-//        System.out.println("Enter the number you want to reverse: ");
-//        int n = scanner.nextInt();
-//    }
-//}
+package hus.oop.lab01.exercises_on_loop_and_decisions;
+
+import java.util.Scanner;
+public class ExtractDigits {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Enter the number you want to reverse: ");
+        int n = scanner.nextInt();
+        while (n > 0) {
+            int digit = n % 10;
+            System.out.print(digit + " ");
+            n /= 10;
+        }
+    }
+}
